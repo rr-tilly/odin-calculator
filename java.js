23643deccb3fd4dfdb4d9515c4a9c8b4a1f560d6
rr-tilly <88960346+rr-tilly@users.noreply.github.com>
2025-02-14 09:51:29 +0800

    allow users to click buttons for basic functions

--- java.js
+++ java.js
@@ -11,21 +11,85 @@ function divide(a, b) {
     return a / b
 }
 
-function operate(operator, a, b) {
+function operate(expression) {
+    console.log(expression);
+    console.log(typeof expression);
+
+    const regexp = /(\d+|[a-z]+)/g;
+
+    const array = expression.split(" ");
+    console.table(array);
+
+    const operator = array[1];
+    console.log(operator);
+
+    const a = Number(array[0]);
+    const b = Number(array[2]);
 
     switch (operator) {
         case "+":
-            return add(a, b);
-
+            updateResult(add(a, b));
+            break;
         case "-":
-            return subtract(a, b)
-
-        case "*":
-            return multiply(a, b)
-
-        case "/":
-            return divide(a, b)
-
+            updateResult(subtract(a, b));
+            break;
+        case "x":
+            updateResult(multiply(a, b));
+            break;
+        case "÷":
+            updateResult(divide(a, b));
+            break;
     }
 
 }
+
+const allKeys = document.querySelectorAll(".keys");
+allKeys.forEach(element => {
+    element.addEventListener("click", element => updateDisplay(element.target.id))
+});
+
+function updateDisplay(id) {
+    const display = document.querySelector(".operation");
+    const char = document.querySelector(`#${id}`).textContent;
+    switch (id) {
+        case "c":
+            display.textContent = "";
+            const resultDisplay = document.querySelector(".result");
+            resultDisplay.textContent = "";
+            break;
+        case "backspace":
+            const textDisplay = display.textContent;
+            const deleteChar = textDisplay.charAt(textDisplay.length - 1);
+
+            console.log(deleteChar);
+            console.log(typeof deleteChar);
+
+            if (deleteChar === " ") {
+                display.textContent = textDisplay.substring(0, textDisplay.length - 3);
+            } else {
+                display.textContent = textDisplay.substring(0, textDisplay.length - 1);
+            }
+
+            break;
+        case "equals":
+            operate(display.textContent);
+            break;
+        case "plus":
+        case "minus":
+        case "multiply":
+        case "divide":
+            display.textContent = display.textContent + " " + char + " ";
+            break;
+        default:
+            console.log(char);
+            display.textContent = display.textContent + char;
+            break;
+    }
+}
+
+
+function updateResult(result) {
+    resultDisplay = document.querySelector(".result");
+    resultDisplay.textContent = result;
+}
+
